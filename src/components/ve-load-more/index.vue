78f9ba84afc242576b051b4f46418312401fbb1a
LLFY <1305156911@qq.com>
2024-03-16 16:57:41 +0800

    element-plus-pro

---
+++ src/components/ve-load-more/index.vue
@@ -0,0 +1,33 @@
+<!--
+加载更多
+-->
+<script lang="ts" setup>
+import {RefreshLeft} from "@element-plus/icons-vue";
+
+const prop = defineProps({
+  show: {
+    type: Boolean,
+    required: false,
+    default: () => true
+  }
+})
+const emits = defineEmits(['handleClick'])
+const loadMore = async () => {
+  emits('handleClick')
+}
+</script>
+
+<template>
+  <div class="load-more">
+    <el-link v-show="show" :icon="RefreshLeft" :underline="false" type="primary" @click="loadMore">
+      加载更多
+    </el-link>
+    <el-text v-show="!show" type="info">到底了</el-text>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.load-more {
+  text-align: center;
+}
+</style>
