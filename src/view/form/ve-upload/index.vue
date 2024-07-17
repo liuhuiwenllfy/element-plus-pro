4a0ee1ca850b070f1f6528d3a5ce2c309f2d851b
LLFY <1305156911@qq.com>
2024-07-17 23:25:56 +0800

    element-plus-pro

--- src/view/form/ve-upload/index.vue
+++ src/view/form/ve-upload/index.vue
@@ -5,8 +5,6 @@ import VeBatchUploadPicture from '@/components/ve-upload/ve-batch-upload-picture
 import VeUploadAvatar from '@/components/ve-upload/ve-upload-avatar/index.vue'
 import VeUploadPicture from '@/components/ve-upload/ve-upload-picture/index.vue'
 import code from './index.md?raw'
-import {UploadFile} from "element-plus";
-import {ref} from "vue";
 
 const stats = [
   {
@@ -72,40 +70,19 @@ const incident = [
     callback: 'file',
   },
 ]
-const file = ref()
-const handleUploadSuccess = (response: any, uploadFile: UploadFile) => {
-  console.log(response.data)
-  file.value = response.data
-}
 </script>
 
 <template>
   <ve-page id="ve-upload" :code="code" :incident="incident" :stats="stats" title="ve-upload 上传">
     <template #default>
       <h3>批量上传文件</h3>
-      <ve-batch-upload-file
-          api="/single-upload/gridFsFile/uploadTempFile"
-          authorization="Bearer eyJraWQiOiI1ZWM3MGFlMWQ3M2Y0NmRjYmMwZjY5Yzg3NjE2OWYyMCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJzeXNhZG1pbiIsImF1ZCI6Im1lc3NhZ2luZy1jbGllbnQiLCJuYmYiOjE3MjEyMjkzNTcsInNjb3BlIjpbImFsbCJdLCJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMTIiLCJleHAiOjE3MjEyMzY1NTcsImlhdCI6MTcyMTIyOTM1N30.b0tETuGkNBhXJOrBZ1wObmAQdotbcZUmlA5ZMyx4tZDZuJqIf1GOWMFtMXyiQOgI4bEsfAs1ellykkHMpYK9Agrc-NDjfjZbUzD4WokNEPbc-6TcF54z4uYQFKB67dwx3DRFmF7seWK8xHVElsZn-giUn19IHkxucYoWf7ZV374P5Heggr8ryUwGtv1P013n8ICcMrfPWCKuQZ5vIjWbuYXPMRuvIoGAKD_B8y5XPIDUsiuvcN5JEvUK_RAn44D4tNHS-EnVbsVR7rC6IukcArpIaQZNC7EktDEOHvf2IGJbuL6ngPVraxmXLU_5pFAwh82YczO228v8c_4cXuBGRA"
-      />
+      <ve-batch-upload-file api="#"/>
       <h3>批量上传图片</h3>
-      <ve-batch-upload-picture
-          api="/single-upload/gridFsFile/uploadTempFile"
-          authorization="Bearer eyJraWQiOiI1ZWM3MGFlMWQ3M2Y0NmRjYmMwZjY5Yzg3NjE2OWYyMCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJzeXNhZG1pbiIsImF1ZCI6Im1lc3NhZ2luZy1jbGllbnQiLCJuYmYiOjE3MjEyMjkzNTcsInNjb3BlIjpbImFsbCJdLCJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMTIiLCJleHAiOjE3MjEyMzY1NTcsImlhdCI6MTcyMTIyOTM1N30.b0tETuGkNBhXJOrBZ1wObmAQdotbcZUmlA5ZMyx4tZDZuJqIf1GOWMFtMXyiQOgI4bEsfAs1ellykkHMpYK9Agrc-NDjfjZbUzD4WokNEPbc-6TcF54z4uYQFKB67dwx3DRFmF7seWK8xHVElsZn-giUn19IHkxucYoWf7ZV374P5Heggr8ryUwGtv1P013n8ICcMrfPWCKuQZ5vIjWbuYXPMRuvIoGAKD_B8y5XPIDUsiuvcN5JEvUK_RAn44D4tNHS-EnVbsVR7rC6IukcArpIaQZNC7EktDEOHvf2IGJbuL6ngPVraxmXLU_5pFAwh82YczO228v8c_4cXuBGRA"
-          is-cropper/>
+      <ve-batch-upload-picture api="#" is-cropper/>
       <h3>上传头像</h3>
-      <ve-upload-avatar
-          api="/single-upload/gridFsFile/uploadTempFile"
-          authorization="Bearer eyJraWQiOiI1ZWM3MGFlMWQ3M2Y0NmRjYmMwZjY5Yzg3NjE2OWYyMCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJzeXNhZG1pbiIsImF1ZCI6Im1lc3NhZ2luZy1jbGllbnQiLCJuYmYiOjE3MjEyMjkzNTcsInNjb3BlIjpbImFsbCJdLCJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMTIiLCJleHAiOjE3MjEyMzY1NTcsImlhdCI6MTcyMTIyOTM1N30.b0tETuGkNBhXJOrBZ1wObmAQdotbcZUmlA5ZMyx4tZDZuJqIf1GOWMFtMXyiQOgI4bEsfAs1ellykkHMpYK9Agrc-NDjfjZbUzD4WokNEPbc-6TcF54z4uYQFKB67dwx3DRFmF7seWK8xHVElsZn-giUn19IHkxucYoWf7ZV374P5Heggr8ryUwGtv1P013n8ICcMrfPWCKuQZ5vIjWbuYXPMRuvIoGAKD_B8y5XPIDUsiuvcN5JEvUK_RAn44D4tNHS-EnVbsVR7rC6IukcArpIaQZNC7EktDEOHvf2IGJbuL6ngPVraxmXLU_5pFAwh82YczO228v8c_4cXuBGRA"
-          :file="file"
-          is-cropper
-          @handle-success="handleUploadSuccess"/>
+      <ve-upload-avatar api="#" is-cropper/>
       <h3>上传图片</h3>
-      <ve-upload-picture
-          api="/single-upload/gridFsFile/uploadTempFile"
-          authorization="Bearer eyJraWQiOiI1ZWM3MGFlMWQ3M2Y0NmRjYmMwZjY5Yzg3NjE2OWYyMCIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJzeXNhZG1pbiIsImF1ZCI6Im1lc3NhZ2luZy1jbGllbnQiLCJuYmYiOjE3MjEyMjkzNTcsInNjb3BlIjpbImFsbCJdLCJpc3MiOiJodHRwOi8vMTI3LjAuMC4xOjgwMTIiLCJleHAiOjE3MjEyMzY1NTcsImlhdCI6MTcyMTIyOTM1N30.b0tETuGkNBhXJOrBZ1wObmAQdotbcZUmlA5ZMyx4tZDZuJqIf1GOWMFtMXyiQOgI4bEsfAs1ellykkHMpYK9Agrc-NDjfjZbUzD4WokNEPbc-6TcF54z4uYQFKB67dwx3DRFmF7seWK8xHVElsZn-giUn19IHkxucYoWf7ZV374P5Heggr8ryUwGtv1P013n8ICcMrfPWCKuQZ5vIjWbuYXPMRuvIoGAKD_B8y5XPIDUsiuvcN5JEvUK_RAn44D4tNHS-EnVbsVR7rC6IukcArpIaQZNC7EktDEOHvf2IGJbuL6ngPVraxmXLU_5pFAwh82YczO228v8c_4cXuBGRA"
-          :file="file"
-          is-cropper
-          @handle-success="handleUploadSuccess"/>
+      <ve-upload-picture api="#" is-cropper/>
     </template>
   </ve-page>
 </template>
